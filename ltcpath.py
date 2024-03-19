f8e074d51d70fe3956c6a0140928abdb8e595e82
taka-4602 <116678390+taka-4602@users.noreply.github.com>
2024-03-19 17:27:49 +0900

    Add files via upload

---
+++ ltcpath.py
@@ -0,0 +1,13 @@
+TOKEN = ''
+timeout=300
+activity=""
+adminlist=['614025927877197834','','']
+path="C:/Users/TAKA/Documents/data/"
+
+text="num.txt"
+logj="log.json"
+mane="maneron.json"
+black="black.json"
+block="block.json"
+
+logincom="</login:>"
